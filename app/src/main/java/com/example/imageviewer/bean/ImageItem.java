ec9a154b519100a841f0818851f9c4ea306ed7d0
even li <chcpbkn@163.com>
2022-07-25 16:44:12 +0800

    feat(bean):增加了bean类ImageItem,作为传入适配器的数据类

---
+++ app/src/main/java/com/example/imageviewer/bean/ImageItem.java
@@ -0,0 +1,24 @@
+package com.example.imageviewer.bean;
+
+import android.graphics.Bitmap;
+
+/**
+ * @author EvenLi
+ */
+public class ImageItem {
+    /**
+     * 第一张图的数据
+     */
+    String uriString1;
+    Bitmap bitmap1;
+    String author1;
+
+
+    /**
+     * 第二张图的数据
+     */
+
+    String uriString2;
+    Bitmap bitmap2;
+    String author2;
+}
